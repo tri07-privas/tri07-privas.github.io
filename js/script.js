2538157ad8bac576a7df5b564e5a4062adc97dfb
Thibaut Soriano <thibautsoriano@gmail.com>
2026-03-07 23:39:59 +0100

    some improvements

--- js/script.js
+++ js/script.js
@@ -1,2 +1,8 @@
 
 console.log("Site TRI07 chargé");
+
+// Si l'URL contient index.html, rediriger vers la racine
+if (window.location.pathname.endsWith('/index.html')) {
+    const cleanPath = window.location.pathname.replace('index.html', '');
+    window.history.replaceState(null, '', cleanPath); // Change l'URL sans recharger
+}
