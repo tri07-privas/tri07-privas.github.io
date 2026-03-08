622529180945488be457de8f599f3587947bff80
Thibaut Soriano <thibautsoriano@gmail.com>
2026-03-08 19:39:02 +0100

    make header sticky and improve blur mobile menu

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ console.log("Site TRI07 chargé");
 // nettoyage URL
 if (window.location.pathname.endsWith("/index.html")) {
   const cleanPath = window.location.pathname.replace("index.html","");
-  window.history.replaceState(null,"",cleanPath);
+  window.history.replaceState(null, "", cleanPath);
 }
 
 /* chargement du header */
@@ -20,39 +20,38 @@ fetch("/includes/header.html")
 
 
 function initMenu() {
-
   const hamburger = document.getElementById("hamburger");
   const navMenu = document.getElementById("nav-menu");
   const overlay = document.getElementById("menu-overlay");
 
   if (!hamburger) return;
 
-  hamburger.addEventListener("click", () => {
+  function closeMenu() {
+    navMenu.classList.remove("open");
+    hamburger.classList.remove("active");
+    overlay.classList.remove("active");
+  }
+
+  hamburger.addEventListener("click", (e) => {
+    e.stopPropagation();
 
     navMenu.classList.toggle("open");
     hamburger.classList.toggle("active");
     overlay.classList.toggle("active");
-
   });
 
-  overlay.addEventListener("click", () => {
-
-    navMenu.classList.remove("open");
-    hamburger.classList.remove("active");
-    overlay.classList.remove("active");
-
-  });
+  overlay.addEventListener("click", closeMenu);
 
   document.querySelectorAll(".nav-menu a").forEach(link => {
-
-    link.addEventListener("click", () => {
-
-      navMenu.classList.remove("open");
-      hamburger.classList.remove("active");
-      overlay.classList.remove("active");
-
-    });
-
+    link.addEventListener("click", closeMenu);
   });
 
+  document.addEventListener("click", (event) => {
+    const isClickInsideMenu = navMenu.contains(event.target);
+    const isClickOnHamburger = hamburger.contains(event.target);
+
+    if (!isClickInsideMenu && !isClickOnHamburger) {
+      closeMenu();
+    }
+  });
 }
